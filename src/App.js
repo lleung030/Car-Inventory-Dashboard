0e720e425d2d8badb58ab1a31ffba9de9fecc7d3
user <user@Users-Mac.local>
2022-12-22 20:24:38 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { useState, useContext } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import Inventory from './components/Inventory'
 import Profile from './components/Profile'
@@ -10,39 +10,49 @@ import Blog from './views/Blog';
 import BlogSingle from './views/BlogSingle';
 import Log from './views/Log';
 import LogSingle from './views/LogSingle';
-
+import { AuthContext } from './contexts/AuthProvider'
 
 
 
 export default function App() {
+  const { login, logout, user } = useContext(AuthContext)
   return (
     <div className="App">
+      <h2>Loggedin User: {user.username}</h2>
       <BrowserRouter>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/inventory">Inventory</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/details">Details</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/log">Log</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/inventory" element={<Inventory />}/>
-        <Route path="/profile" element={<Profile />}/>
-        <Route path="/details" element={<Details />}/>
-        <Route path="/blog">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/inventory">Inventory</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/details">Details</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/log">Log</Link></li>
+            {
+              (user.loggedIn) ?
+                <li><button onClick={logout}>Logout</button></li> :
+                <li><button onClick={login}>Login</button></li>
+            }
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/blog">
             <Route path=":id" element={<BlogSingle />}></Route>
             <Route path="" element={<Blog />} />
           </Route>
 
-        <Route path="/log">
-            <Route path=":id" element={<LogSingle />}></Route>
-            <Route path="" element={<Log />} />
+          <Route path="/log">
+            <Route path=":uid">
+              <Route path=":id" element={<LogSingle />}>
+              </Route>
+              <Route path="" element={<Log />} />
+            </Route>
           </Route>
-      </Routes>
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/contexts/DataProvider.js
+++ src/contexts/DataProvider.js
@@ -1,11 +1,13 @@
 import { useState, useEffect, createContext, useContext } from 'react'
-import { getFirestore, getDocs, collection, doc, getDoc, Timestamp, addDoc, orderBy, query, limit } from '@firebase/firestore'
+import { getFirestore, getDocs, collection, collectionGroup, doc, getDoc, Timestamp, addDoc, orderBy, query, limit, setDoc } from '@firebase/firestore'
+import { AuthContext } from './AuthProvider'
 
 export const DataContext = createContext()
 
 export const DataProvider = function(props) {
     const db = getFirestore()
     const [posts, setPosts] = useState([])
+    const { user } = useContext(AuthContext)
 
     useEffect(() => {
         async function getPosts() {
@@ -15,32 +17,45 @@ export const DataProvider = function(props) {
             // console.log(q)
 
             // const querySnapshot = await getDocs(q)
-            
-            const querySnapshot = await getDocs(collection(db, 'posts'))
+            const q = query(collectionGroup(db, 'posts'))
+
+            const querySnapshot = await getDocs(q)
+            // const querySnapshot = await getDocs(collection(db, 'posts'))
             // console.log(querySnapshot)
             const postDocs = []
             
-            querySnapshot.forEach((doc) => {
+            querySnapshot.forEach(async (doc) => {
+                
+                const userData = await getDoc(doc.ref.parent.parent)
+                const username = userData.data().username
+
                 postDocs.push({
                     id: doc.id,
+                    uid: userData.id,
+                    username: username,
                     ...doc.data()
                 })
                 // console.log(doc.data())
-        })
+                setPosts(postDocs)
+            }) 
         //     console.log(postDocs)
-            setPosts(postDocs)
         }
 
         getPosts()
     }, [])
 
-    async function loadPost(id) {
-        const docRef = doc(db, 'posts', id)
+    async function loadPost(uid, id) {
+        const docRef = doc(db, 'users', uid, 'posts', id)
         const docSnap = await getDoc(docRef)
+
+        const userData = await getDoc(docSnap.ref.parent.parent)
+        const username = userData.data().username
 
         if (docSnap.exists()) {
             return {
                 id: docSnap.id,
+                uid: uid,
+                username: username,
                 ...docSnap.data()
             }
         } else {
@@ -52,13 +67,16 @@ export const DataProvider = function(props) {
         const newPost = {
             title: title,
             body: body,
-            username: 'ctemple',
             date_created: Timestamp.now()
         }
 
-        const doc = await addDoc(collection(db, 'posts'), newPost)
+        const userDoc = await setDoc(doc(db, 'users', user.uid), {
+            username: user.username
+        })
 
-        newPost.id = doc.id
+        const postDoc = await addDoc(collection(db, 'users', user.uid, 'posts'), newPost)
+
+        newPost.id = postDoc.id
         
         setPosts([newPost, ...posts])
     }
